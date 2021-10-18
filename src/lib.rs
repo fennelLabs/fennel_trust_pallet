b2e4ab09c4479c0d025da7b5f0355b1081d30be7
Sean Batzel <romulus108@protonmail.com>
2021-10-18 14:05:31 -0400

    Almost ported to Substrate 3.0.

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,65 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-//!
-//!
-//! Three Levels of Trust:
-//!
-//! - I know you and I trust you (in Trusted List)
-//! - I don't know anything about you so I don't trust you by default  (Not in revoked or trusted list)
-//! - I know you and I definitely don't trust you (in the Revoked List)
-//!
-//! 
+pub use frame_system::pallet::*;
+pub use frame_support::storage::*;
 
-use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch,
-					traits::Get, storage::IterableStorageDoubleMap
-};
-use frame_system::ensure_signed;
+#[cfg(test)]
+mod mock;
 
-pub trait Trait: frame_system::Trait {
-	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
-}
+#[cfg(test)]
+mod tests;
 
-decl_storage! {
-	trait Store for Module<T: Trait> as TrustModule {
-		CurrentIssued get(fn value1): u32;
-		TrustIssuance get(fn key1): double_map hasher(blake2_128_concat) T::AccountId, hasher(blake2_128_concat) u32 => T::AccountId;
-		CurrentRevoked get(fn value2): u32;
-		TrustRevocation get(fn key2): double_map hasher(blake2_128_concat) T::AccountId, hasher(blake2_128_concat) u32 => T::AccountId;
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 	}
-}
 
-decl_event!(
-	pub enum Event<T>
-	where
-		AccountId = <T as frame_system::Trait>::AccountId,
-	{
-		TrustIssued(AccountId, AccountId),
-		TrustRevoked(AccountId, AccountId),
-		TrustIssuanceRemoved(AccountId, AccountId),
-		TrustRevocationRemoved(AccountId, AccountId),
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::storage]
+	#[pallet::getter(fn value1)]
+	pub type CurrentIssued<T: Config> = StorageValue<_, u32>;
+	#[pallet::storage]
+	#[pallet::getter(fn key1)]
+	pub type TrustIssuance<T: Config> = StorageDoubleMap<_, Blake2_128Concat, T::AccountId, Blake2_128Concat, u32, T::AccountId>;
+	#[pallet::storage]
+	#[pallet::getter(fn value2)]
+	pub type CurrentRevoked<T: Config> = StorageValue<_, u32>;
+	#[pallet::storage]
+	#[pallet::getter(fn key2)]
+	pub type TrustRevocation<T: Config> = StorageDoubleMap<_, Blake2_128Concat, T::AccountId, Blake2_128Concat, u32, T::AccountId>;
+
+	#[pallet::event]
+	#[pallet::metadata(T::AccountId = "AccountId")]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		TrustIssued(T::AccountId, T::AccountId),
+		TrustRevoked(T::AccountId, T::AccountId),
+		TrustIssuanceRemoved(T::AccountId, T::AccountId),
+		TrustRevocationRemoved(T::AccountId, T::AccountId),
 	}
-);
 
-decl_error! {
-	pub enum Error for Module<T: Trait> {
+	#[pallet::error]
+	pub enum Error<T> {
 		NoneValue,
 		StorageOverflow,
 	}
-}
 
-decl_module! {
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		type Error = Error<T>;
-
-		fn deposit_event() = default;
-
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
 		/// Fully give your trust to an account 
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn issue_trust(origin, address: T::AccountId) -> dispatch::DispatchResult {
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn issue_trust(origin: OriginFor<T>, address: T::AccountId) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			// WARN: THIS ITERATION IS VERY INEFFICIENT
 			// NOT SUITABLE FOR PRODUCTION
@@ -68,17 +72,17 @@ decl_module! {
 
 			if do_insert {
 				<TrustIssuance<T>>::insert(&who, i, &address);
-				let total: u32 = CurrentIssued::get();
-				CurrentIssued::put(total + 1);
-				Self::deposit_event(RawEvent::TrustIssued(who, address));
+				let total: u32 = <CurrentIssued<T>>::get().unwrap();
+				<CurrentIssued<T>>::put(total + 1);
+				Self::deposit_event(Event::TrustIssued(who, address));
 			}
 			
 			Ok(())
 		}
 
 		/// Remove issued trust from an account, making their trust status 'Unknown'
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn remove_trust(origin, address: T::AccountId) -> dispatch::DispatchResult {
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn remove_trust(origin: OriginFor<T>, address: T::AccountId) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			let mut do_remove: Option<u32> = None;
 			for (index, issued) in <TrustIssuance<T>>::iter_prefix(&who) {
@@ -87,17 +91,17 @@ decl_module! {
 
 			if let Some(index) = do_remove {
 				<TrustIssuance<T>>::remove(&who, index);
-				let key = CurrentIssued::get();
-				CurrentIssued::put(key - 1);
-				Self::deposit_event(RawEvent::TrustIssuanceRemoved(address, who));
+				let key = <CurrentIssued<T>>::get().unwrap();
+				<CurrentIssued<T>>::put(key - 1);
+				Self::deposit_event(Event::TrustIssuanceRemoved(address, who));
 			}
 
 			Ok(())
 		}
 
 		/// Revoke trust from an account
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn revoke_trust(origin, address: T::AccountId) -> dispatch::DispatchResult {
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn revoke_trust(origin: OriginFor<T>, address: T::AccountId) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 
 			let mut do_insert = true;
@@ -109,16 +113,16 @@ decl_module! {
 
 			if do_insert {
 				<TrustRevocation<T>>::insert(&who, i, &address);
-				let key: u32 = CurrentRevoked::get();	
-				CurrentRevoked::put(key + 1);
-				Self::deposit_event(RawEvent::TrustRevoked(address, who));
+				let key: u32 = <CurrentRevoked<T>>::get().unwrap();	
+				<CurrentRevoked<T>>::put(key + 1);
+				Self::deposit_event(Event::TrustRevoked(address, who));
 			}
 		
 			Ok(())
 		}
 
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn remove_revoked_trust(origin, address: T::AccountId) -> dispatch::DispatchResult {
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn remove_revoked_trust(origin: OriginFor<T>, address: T::AccountId) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 
 			let mut do_remove: Option<u32> = None;
@@ -129,9 +133,9 @@ decl_module! {
 			
 			if let Some(index) = do_remove {
 				<TrustRevocation<T>>::remove(&who, index);	
-				let key: u32 = CurrentRevoked::get();
-				CurrentRevoked::put(key - 1);
-				Self::deposit_event(RawEvent::TrustRevocationRemoved(address, who));
+				let key: u32 = <CurrentRevoked<T>>::get().unwrap();
+				<CurrentRevoked<T>>::put(key - 1);
+				Self::deposit_event(Event::TrustRevocationRemoved(address, who));
 			}
 
 			Ok(())
